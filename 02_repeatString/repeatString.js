c896946ba390aea8810080b4db68e347f3fe9bba
Harsh <harshsharma41664@gmail.com>
2022-10-04 18:14:42 +0530

    questions

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str,num) {
+    if (num===0)
+        return '';
+    if (num<0)
+        return "ERROR";
+    let strm=str;
+    for (i=0;i<num-1;i++)
+        str+=strm;
+    return str;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
 const removeFromArray = function() {
+    let arr = arguments[0];
+    for (i=1;i<arguments.length;++i){
+        let pos = arr.indexOf(arguments[i]);
+        if (pos===-1)
+            continue;
+        arr.splice(pos,1);
+    }
 
+    return arr;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
