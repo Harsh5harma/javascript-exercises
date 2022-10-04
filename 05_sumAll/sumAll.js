e55e095db9ce128343c587756e40a9a6c7d13955
Harsh <harshsharma41664@gmail.com>
2022-10-04 18:13:51 +0530

    did questions

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(start,end) {
+    if (!Number.isInteger(end))
+        return "ERROR";
+    let min=0;
+    let max=0;
+    if (start>end){
+        min = end;
+        max = start;
+    }else{
+        min = start;
+        max = end;
+    }
+    
+    if (min<0||max<0)
+        return "ERROR";
+    let sum=0;
+    for (i=min;i<=max;++i){
+        sum+=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
