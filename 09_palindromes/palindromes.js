69a0b63efeb171317bb334d3bbd1f27f9bfd6e41
Harsh <harshsharma41664@gmail.com>
2022-10-10 02:26:02 +0530

    palindrome done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let rstr="";
+    let nstr="";
+    for (let i =0;i<str.length;++i){
+        if (str[i]===' '||str[i]===','||str[i]==='.'||str[i]==='!')
+            continue;
+        else
+            nstr+=str[i].toLowerCase();
+    }
+    for (let i =nstr.length-1;i>=0;--i){
+        rstr+=nstr[i];
+    }
+    return rstr===nstr;
 };
 
 // Do not edit below this line
