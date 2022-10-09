6a672a6e6005d2becac0859e8c9c95d559421428
Harsh <harshsharma41664@gmail.com>
2022-10-10 02:48:12 +0530

    done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = Number(n);
+    if (n<0)
+        return "OOPS";
+    let a = 0;
+    let b = 1;
+    let c = a+b;
+    for (let i =0;i<n-2;++i){
+        a=b;
+        b=c;
+        c=a+b;
+    }
+    return c;
 
 };
 
