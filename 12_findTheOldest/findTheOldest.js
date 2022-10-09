d09b44f5b847991756f875180c6c84a7441430b2
Harsh <harshsharma41664@gmail.com>
2022-10-10 04:22:10 +0530

    done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+    let oldest = {};
+    let maxAge = 0;
+    people.forEach((person) => {
+        if (person.yearOfDeath === undefined) {
+            let age = new Date().getFullYear()- person.yearOfBirth;
+            if (age > maxAge) {
+                maxAge = age;
+                oldest = person;
+            }
+        }
+        else {
+            let age = person.yearOfDeath - person.yearOfBirth;
+            if (age > maxAge) {
+                maxAge = age;
+                oldest = person;
+            }
+        }
+    });
+    return oldest;
 };
 
 // Do not edit below this line
