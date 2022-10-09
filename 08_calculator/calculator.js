39c06324b4ab5ce7192a63941bfd699080d44524
Harsh <harshsharma41664@gmail.com>
2022-10-10 02:14:19 +0530

    thuid

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(addTT) {
+      let sum = 0;
+      for (let i =0;i<addTT.length;++i)
+        sum+=addTT[i];
+      return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(mulTT) {
+    let prod=1;
+    for (let i = 0;i<mulTT.length;++i)
+      prod*=mulTT[i];
+    return prod;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let fact=1;
+  for (let i = 1;i<=n;++i)
+    fact*=i;
+  return fact;
 };
 
 // Do not edit below this line
